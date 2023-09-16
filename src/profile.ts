337438877dff82e5a9a34adedaab53de0792d9b8
Plumbiu <3434909403@qq.com>
2023-09-16 21:27:51 +0800

    feat: add contributions field

--- src/profile.ts
+++ src/profile.ts
@@ -1,4 +1,4 @@
-import { initCheerio } from './utils.js'
+import { initCheerio, parseDate } from './utils.js'
 
 // TODO: try find a better way to get the profile infomation
 enum Profile {
@@ -7,6 +7,17 @@ enum Profile {
   Details = '.vcard-detail',
   Organization = '.avatar-group-item',
   Sponsor = '#sponsors-section-list > div',
+  Contributions = '.js-yearly-contributions',
+}
+
+interface Contributions {
+  amount: string
+  full: Array<{
+    date: string
+    level: string
+    count: string
+  }>
+  org: string[]
 }
 
 type NormalObj = Record<string, string>
@@ -14,9 +25,14 @@ type NormalObj = Record<string, string>
   get the profile infomation
 */
 export async function profileInfo(user: string) {
+  // const $ = await initCheerio(`http://127.0.0.1:5500/${user}.html`)
   const $ = await initCheerio(`https://github.com/${user}`)
   const bio = $(Profile.Bio).text()
   // TODO: we can make every field into a function
+  // follow: {
+  //   followers: '2.9k',
+  //   floowing: '175'
+  // }
   const follow: NormalObj = {}
   $(Profile.Follow).each((i, el) => {
     /*
@@ -26,24 +42,91 @@ export async function profileInfo(user: string) {
     const key = i === 0 ? 'followers' : 'following'
     follow[key] = $(el).text().replace(/\s|\n/g, '')
   })
-
+  // details: {
+  //   homeLocation: 'Hangzhou,China',
+  //   url: 'https://blog.plumbiu.club'
+  //   ...
+  // }
   const details: NormalObj = {}
   $(Profile.Details).each((_i, el) => {
     const key = $(el).attr('itemprop') as string
     details[key] = $(el).text().replace(/\s|\n/g, '')
   })
-
+  // organizations: {
+  //   vuejs: 'https://github.com/vuejs',
+  //   vitejs: 'https://github.com/vitejs',
+  //   ...
+  // }
   const organizations: NormalObj = {}
   $(Profile.Organization).each((_i, el) => {
     const key = $(el).attr('aria-label') as string
     organizations[key] = `https://github.com${$(el).attr('href')}`
   })
-
+  // sponsor: {
+  //   current: {
+  //     '@wey-gu': 'https://avatars.githubusercontent.com/u/1651790?s=60&v=4',
+  //     '@sodatea': 'https://avatars.githubusercontent.com/u/3277634?s=60&v=4',
+  //      ...
+  //   },
+  //   past: {
+  //     '@element-plus': 'https://avatars.githubusercontent.com/u/68583457?s=60&v=4',
+  //     '@kallydev': 'https://avatars.githubusercontent.com/u/36319157?s=60&v=4'
+  //      ...
+  //   }
+  // },
   const sponsor = await sponsorInfo(user)
-
+  // contributions: {
+  //   amount: '2499',
+  //   full: [
+  //     { date: '2022-9-11', level: '1', count: '5' },
+  //     { date: '2022-9-11', level: '1', count: '5' },
+  //     ...
+  //   ]
+  // }
+  const contributions: Contributions = {
+    amount: '0',
+    full: [],
+    org: [],
+  }
+  $(Profile.Contributions).each((_i, rootEl) => {
+    // contributions amount
+    contributions.amount = /[\d,]+/
+      .exec($(rootEl).find('h2').text())![0]
+      .replace('', '')
+    // date contributions information
+    $(rootEl)
+      .find('td')
+      .each((_i, el) => {
+        const elm = $(el)
+        const date = elm.attr('data-date')
+        const level = elm.attr('data-level') ?? '0'
+        let count = elm.text().split(' ')[0]
+        if (!Number(count)) {
+          count = '0'
+        }
+        if (date) {
+          contributions.full.push({
+            date,
+            level,
+            count,
+          })
+        }
+      })
+    $(rootEl)
+      .find('.js-org-filter-link')
+      .each((_i, el) => {
+        const text = $(el).text()
+        if (text) {
+          contributions.org.push(text.trim())
+        }
+      })
+    contributions.full.sort((a, b) => {
+      return parseDate(a.date, b.date)
+    })
+  })
   /*
     More filed to do:
-    TODO: sponsorging, pinned, contributions, activity, repositories, language-use
+    TODO: sponsorging, pinned, activity, repositories, language-use
     TODO: stars, stars-list, hightlights
   */
   return {
@@ -52,6 +135,7 @@ export async function profileInfo(user: string) {
     sponsor,
     details,
     organizations,
+    contributions,
   }
 }
 
@@ -69,23 +153,32 @@ export async function sponsorInfo(user: string) {
     as the class name in the label is duplicated
     and past sponsor will not show in the profile page.
   */
-  const $ = await initCheerio(`https://github.com/sponsors/${user}`)
   const sponsor: Sponsor = {
     current: {},
     past: {},
   }
-  $(Profile.Sponsor).each((i, rootEl) => {
-    $(rootEl)
-      .find('img')
-      .each((_i, el) => {
-        /*
-          As the `follow` field,
-          current and past sponsor doesn't indicated by the sponsor page
-        */
-        const prefix = i === 0 ? 'current' : 'past'
-        const key = $(el).attr('alt')!
-        sponsor[prefix][key] = $(el).attr('src')!
-      })
-  })
-  return sponsor
+  try {
+    const $ = await initCheerio(`https://github.com/sponsors/${user}`)
+    $(Profile.Sponsor).each((i, rootEl) => {
+      $(rootEl)
+        .find('img')
+        .each((_i, el) => {
+          /*
+            As the `follow` field,
+            current and past sponsor doesn't indicated by the sponsor page
+          */
+          const prefix = i === 0 ? 'current' : 'past'
+          const key = $(el).attr('alt')!
+          sponsor[prefix][key] = $(el).attr('src')!
+        })
+    })
+  } finally {
+    // eslint-disable-next-line no-unsafe-finally
+    return sponsor
+  }
 }
+
+// void profileInfo('Plumbiu').then((data) => {
+//   writeFileSync('./Plumbiu.json', JSON.stringify(data))
+//   console.log(data)
+// })
--- src/utils.ts
+++ src/utils.ts
@@ -8,3 +8,15 @@ export async function initCheerio(url: string) {
   const html = await fetchJSON.text()
   return cheerio.load(html)
 }
+
+/*
+  parse date like '2023-03-02'
+*/
+export function parseDate(date1: string, date2: string) {
+  if (date1 === date2) {
+    return 0
+  }
+  const d1 = +date1.split('-').join('')
+  const d2 = +date2.split('-').join('')
+  return d1 - d2
+}
