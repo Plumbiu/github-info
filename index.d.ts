842385efd0f26cf2de41197556b717cc6e00f22e
Plumbiu <3434909403@qq.com>
2023-09-19 14:13:55 +0800

    fix: types support

--- index.d.ts
+++ index.d.ts
@@ -1,135 +1,83 @@
 declare module '@plumbiu/github-info' {
-  interface ReturnedType {
-    name: string
-    bio: string | null
-    company: string | null
-    blog: string | null
-    location: string | null
-    email: string | null
-    avatar: string
-    created_at: string
-    updated_at: string
-    twitter: string
-    public_repos: Array<{
-      id: string
-      name: string
-      fullName: string
-      url: string
-      description: string
-      languate: string
-      star: string
-      wathers: string
-      forks: string
-      issue: string
-      license: string
-      created_at: string
-      updated_at: string
-      pushed_at: string
-      owner: {
-        user: string | undefined
-        avatar: string | undefined
-      }
-    }>
-    followers: Array<{
-      id: string
-      user: string
-      avatar: string
-    }>
-    following: Array<{
-      id: string
-      user: string
-      avatar: string
-    }>
-    starred: Array<{
-      id: string
-      name: string
-      fullName: string
-      url: string
-      description: string
-      language: string
-      star: string
-      watchers: string
-      forks: string
-      issue: string
-      license: string
-      created_at: string
-      updated_at: string
-      pushed_at: string
-      owner: {
-        user: string | undefined
-        avatar: string | undefined
-      }
-    }>
-    events: {
-      id: string
-      type: string
-      created_at: string
-      org: {
-        name: string
+  type User = Record<'name' | 'avatar' | 'created_at' | 'updated_at', string> &
+    Record<
+      'bio' | 'company' | 'blog' | 'location' | 'email' | 'twitter',
+      string | null
+    >
+  type PublicRepos = Record<
+    | 'id'
+    | 'name'
+    | 'fullName'
+    | 'url'
+    | 'description'
+    | 'languate'
+    | 'star'
+    | 'wathers'
+    | 'forks'
+    | 'issue'
+    | 'license'
+    | 'created_at'
+    | 'updated_at'
+    | 'pushed_at',
+    string
+  > & {
+    owner: {
+      user: string | undefined
+      avatar: string | undefined
+    }
+  }
+  type Follow = Record<'id' | 'user' | 'avatar', string>
+  type EventBase = Record<'id' | 'type' | 'created_at' | 'repo', string>
+  type AvatarName = Record<'name' | 'avatar', string>
+  type Commit = Record<'author' | 'message' | 'url', string>
+  type PullRequestBase = Record<
+    | 'html_url'
+    | 'diff_url'
+    | 'patch_url'
+    | 'issue_url'
+    | 'state'
+    | 'locked'
+    | 'title'
+    | 'body'
+    | 'created_at'
+    | 'updated_at'
+    | 'closed_at'
+    | 'merged_by'
+    | 'comments'
+    | 'additions'
+    | 'deletions'
+    | 'changed_files',
+    string
+  > & {
+    merged_at: string | undefined
+  }
+  type PullRequestUser = Record<'name' | 'avatar' | 'url', string | undefined>
+  type PullRequestBase = Record<'label' | 'ref', string | undefined> & {
+    user: PullRequestUser
+  }
+  type ReturnedType = User & {
+    public_repos: PublicRepos[]
+    followers: Follow[]
+    following: Follow[]
+    starred: PublicRepos[]
+    events: EventBase & {
+      org: AvatarName & {
         url: string
-        avatar: string
       }
-      actor: {
-        name: string
-        avatar: string
-      }
-      repo: string
+      actor: AvatarName
       payload: {
         ref: string
-        commits: Array<{
-          author: string
-          message: string
-          url: string
-        }>
+        commits: Commit[]
       }
-      pull_request?: {
-        html_url: string
-        diff_url: string
-        patch_url: string
-        issue_url: string
-        state: string
-        locked: string
-        title: string
-        body: stirng
-        created_at: stirng
-        updated_at: stirng
-        closed_at: stirng
-        merged_by: stirng
-        comments: stirng
-        additions: stirng
-        deletions: stirng
-        changed_files: stirng
-        merged_at: stirng | undefined
-        user: {
-          name: string | undefined
-          avatar: string | undefined
-          url: string | undefined
+      pull_request?: PullRequestBase & {
+        user: PullRequestUser
+        head: PullRequestBase & {
+          repo: Record<
+            'name' | 'fullName' | 'description' | 'language' | 'license',
+            string | undefined
+          >
         }
-        head: {
-          label: string | undefined
-          ref: string | undefined
-          user: {
-            name: string | undefined
-            avatar: string | undefined
-            url: string | undefined
-          }
-          repo: {
-            name: string | undefined
-            fullName: string | undefined
-            description: string | undefined
-            language: string | undefined
-            license: string | undefined
-          }
-        }
-        base: {
-          label: string | undefined
-          ref: string | undefined
-          user: {
-            name: string | undefined
-            avatar: string | undefined
-            url: string | undefined
-          }
-        }
+        base: PullRequestBase
       }
     }
   }
